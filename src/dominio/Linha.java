cb50ff97704234c3bea5963e4a04c56220e6fdf5
Thiago <thiagohcarvalho81@hotmail.com>
2020-06-08 19:57:13 -0300

    Attributes and getters and setters added to the Acessorio e Linha classes

--- src/dominio/Linha.java
+++ src/dominio/Linha.java
@@ -1,5 +1,21 @@
 package dominio;
 
 public class Linha {
+	
+	private String descricao;
+	
+	public Linha() {
+	}
 
+	public Linha(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
